f9a8a4a18fc16e0be0d85ead56f04359e75d4b62
張 <changws@ubint.jp>
2016-04-11 17:42:04 +0900

    First Commit

---
+++ shop/member_login.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>ユビーネット</title>
+</head>
+<body>
+
+<a href="shop_form.html">会員登録</a><br />
+
+会員ログイン<br />
+<br />
+<form method="post" action="member_login_check.php">
+登録メールアドレス<br />
+<input type="text" name="email" ><br />
+パスワード<br />
+<input type="password" name="pass"><br />
+<br />
+<input type="submit" value="ログイン">
+</form>
+</body>
+</html>
